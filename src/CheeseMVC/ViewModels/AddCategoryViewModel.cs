39bcab2bd08f3e0a29d2a83178e3380a59e15efe
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-03-02 00:11:20 -0600

    Added ViewModel - AddCheeseViewModel

--- src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CheeseMVC.ViewModels
 {
     public class AddCategoryViewModel
     {
+        [Required(ErrorMessage = "You must enter a name for the category")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
     }
 }
