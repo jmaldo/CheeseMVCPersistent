e75a1699778442de573c75c700a98a282879d97a
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-03-03 10:57:44 -0600

    Added View/Menu to fix errors

--- src/CheeseMVC/Migrations/20170404203044_InitialMigration.cs
+++ src/CheeseMVC/Migrations/20170404203044_InitialMigration.cs
@@ -9,20 +9,20 @@ namespace CheeseMVC.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Cheeses",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Description = table.Column<string>(nullable: true),
-                    Name = table.Column<string>(nullable: true),
-                    Type = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Cheeses", x => x.ID);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Cheeses",
+            //    columns: table => new
+            //    {
+            //        ID = table.Column<int>(nullable: false)
+            //            .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            //        Description = table.Column<string>(nullable: true),
+            //        Name = table.Column<string>(nullable: true),
+            //        Type = table.Column<int>(nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Cheeses", x => x.ID);
+            //    });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
